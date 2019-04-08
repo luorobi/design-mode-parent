399ef373d02be8a7ac48d90f13af2e680abe19a1
luo.robi <9332223@qq.com>
2019-04-08 16:33:09 +0800

    单一职责原则

---
+++ single-responsibility/src/main/java/com/luocx/singleresponsibility/SingleResponsibilityApplication.java
@@ -0,0 +1,13 @@
+package com.luocx.singleresponsibility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleResponsibilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SingleResponsibilityApplication.class, args);
+    }
+
+}
